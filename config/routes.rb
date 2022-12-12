efbcdb03a174e8040f4d2a77c01ec15fb3a79c3c
MikeSC27 <mcruzen89@gmail.com>
2022-12-12 11:15:26 -0800

    first user story complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "post#index"
+  get 'post' => 'post#index'
+
 end
